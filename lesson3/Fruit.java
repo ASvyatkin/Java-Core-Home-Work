59158cb9f2b6d7e199bb09bef2ce6282f5cffbf2
ASvyatkin <svyatkin-andrey@mail.ru>
2023-04-16 17:05:15 +0300

    home work 3

---
+++ lesson3/Fruit.java
@@ -0,0 +1,13 @@
+package lesson3;
+//Created by: Andrey Svyatkin
+abstract class Fruit {
+    private float weight;
+
+    Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
